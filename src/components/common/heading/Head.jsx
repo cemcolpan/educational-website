b2df879d2dbd2db201214ac1526c5d2bc831c9e1
Cem Colpan <77989101+cemcolpan@users.noreply.github.com>
2022-06-30 18:20:00 +0300

    head header component create

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+ const Head = () => {
+  return (
+    <>
+        <section className='head'>
+            <div className="container flexSB">
+                <div className="logo">
+                    <h1>ACADEMIA</h1>
+                    <span>ONLINE EDUCATION & LEARNING</span>
+                </div>
+                <div className="social">
+                    <i className='fab fa-facebook-f'></i>
+                    <i className='fab fa-instagram'></i>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
