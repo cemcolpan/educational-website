71822c87811912f777737855ed328ba9868df43f
Cem Colpan <77989101+cemcolpan@users.noreply.github.com>
2022-07-01 17:56:12 +0300

    revised

---
+++ src/components/about/AboutCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <>
+        <section className="aboutHome">
+            <div className="container flexSB">
+                <div className="left row">
+                    <img src="./images/about.webp" alt="" />
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default AboutCard
